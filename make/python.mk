9e5cbd4367d4a680271305612a0dcc815f1b197c
Jose Tomas Robles Hahn <jtrobles@cordada.com>
2022-07-27 20:09:15 -0400

    fix: Add missing base Make tasks
    Ref: https://cordada.aha.io/requirements/COMPCLDATA-100-15
    Ref: Git commit ID 8da0e0b7ef4a0027fbf57b1e2c4fd524116b7bca

--- make/python.mk
+++ make/python.mk
@@ -1,7 +1,23 @@
 PYTHON ?= python3
 PYTHON_PIP ?= $(PYTHON) -m pip
+PYTHON_VIRTUALENV_DIR ?= pyenv
+PYTHON_PIP_VERSION_SPECIFIER ?= >=21.1.2
+PYTHON_SETUPTOOLS_VERSION_SPECIFIER ?= >=57.0.0
+PYTHON_WHEEL_VERSION_SPECIFIER ?= >=0.36.2
 PYTHON_PIP_TOOLS_VERSION_SPECIFIER ?= >=6.1.0
 PYTHON_PIP_TOOLS_SRC_FILES ?= requirements.in
+
+.PHONY: python-pip-install
+python-pip-install: ## Install Pip
+	$(PYTHON_PIP) install 'pip$(PYTHON_PIP_VERSION_SPECIFIER)'
+
+.PHONY: python-setuptools-install
+python-setuptools-install: ## Install Setuptools
+	$(PYTHON_PIP) install 'setuptools$(PYTHON_SETUPTOOLS_VERSION_SPECIFIER)'
+
+.PHONY: python-wheel-install
+python-wheel-install: ## Install Wheel
+	$(PYTHON_PIP) install 'wheel$(PYTHON_WHEEL_VERSION_SPECIFIER)'
 
 .PHONY: python-deps-compile
 python-deps-compile: $(patsubst %,python-deps-compile-%,$(PYTHON_PIP_TOOLS_SRC_FILES))
@@ -18,6 +34,10 @@ python-deps-sync-check-%: python-deps-compile-%
 	@# Replace file extension of source Python dependency manifest (.in)
 	@# with file extension of compiled Python dependency manifest (.txt).
 	git diff --exit-code -- "$(*:.in=.txt)"
+
+.PHONY: python-virtualenv
+python-virtualenv: ## Create virtual Python environment
+	$(PYTHON) -m venv "$(PYTHON_VIRTUALENV_DIR)"
 
 .PHONY: python-pip-tools-install
 python-pip-tools-install: ## Install Pip Tools
