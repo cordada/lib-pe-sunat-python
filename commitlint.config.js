a555955ade48c63ec22afe9e52383fcce51cf84a
Jose Tomas Robles Hahn <jtrobles@cordada.com>
2025-01-24 10:44:44 -0300

    chore: Update configuration of Git commit linter
    - Update Commitlint configuration.

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,20 +7,4 @@ Commitlint is a Git commit message linter.
 - Documentation: https://github.com/conventional-changelog/commitlint#readme
 */
 
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-
-  /*
-  Rules
-
-  Documentation:
-  https://github.com/conventional-changelog/commitlint/blob/v17.0.3/docs/reference-rules.md
-  */
-  rules: {
-    "body-max-line-length": [1, "always", 100],
-    "header-max-length": [2, "always", 72],
-    "subject-case": [1, "always", ["sentence-case"]],
-    "trailer-exists": [1, "always", "Ref:"],
-    "type-enum": [2, "always", ["chore", "feat", "fix"]],
-  },
-};
+module.exports = { extends: ["@cordada/commitlint-config-cordada"] };
